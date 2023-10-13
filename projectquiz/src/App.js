6cb7e034c019d439f66cf0c71162c05c9e33eee5
Arun Saravanan <arunsaravanan003@gmail.com>
2023-10-13 19:47:48 +0530

    First Commit

---
+++ projectquiz/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import Quiz from './Components/Quiz';
+
+function App() {
+  return (
+    <div className="App">
+      <Quiz/>
+    </div>
+  );
+}
+
+export default App;
---
+++ projectquiz/src/Data/QuizData.js
@@ -0,0 +1,67 @@
+export const QuizData = [
+    {
+        question: "What country has the most islands?",
+        options: ["Ireland", "Switzerland", "Sweden", "New Zealand"],
+        answer:3
+    },
+
+    {
+        question: "Which planet has the most moons?",
+        options:["Mars", "Venus", "Saturn", "Earth"],
+        answer: 3
+    },
+
+    {
+        question: "What city is known as The Eternal City?",
+        options: ["Rome", "Denver", "Rio", "Manchester"],
+        answer: 1
+    },
+
+    {
+        question: "How many hearts does an octopus have?",
+        options:[ "5", "3", "1", "7"],
+        answer: 2
+    },
+
+    {
+        question: "What year was the United Nations established? ",
+        options:[ "1919", "1895", "1945", "1845"],
+        answer: 3
+    },
+
+    
+    {
+        question: "How many bones do we have in an ear?",
+        options:[ "2", "5", "3", "None"],
+        answer: 3
+    },
+
+    
+    {
+        question: "Which is the only body part that is fully grown from birth?",
+        options:[ "Hair", "Ear", "Nose", "Eyes"],
+        answer: 4
+    },
+
+    
+    {
+        question: "In what country was Elon Musk born?",
+        options:[ "United States", "Australia", "South Africa", "Canada"],
+        answer: 3
+    },
+
+    
+    {
+        question: "What planet is closest to the sun? ",
+        options:[ "Earth", "Mercury", "Jupiter", "Neptune"],
+        answer: 2
+    },
+
+    
+    {
+        question: "What is the capital of Ireland? ",
+        options:[ "Carlow", "Waterford", "Dublin", "Galway"],
+        answer: 3
+    },
+
+];
